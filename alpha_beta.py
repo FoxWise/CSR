2d8cc4f545c7fba4b88694ac3e9f686182e3724c
FoxWise <bohdan96@gmail.com>
2018-06-26 02:18:36 +0300

    added txt output for OriginLab

--- alpha_beta.py
+++ alpha_beta.py
@@ -15,20 +15,35 @@ graph = TGraph2D()
 final = len(file.parameterData[-1])
 
 for n in range (0,final):
-	x =float(file.parameterData[-1][n]) #alpha
-	y =float(file.parameterData[-2][n]) #beta
+	x =float(file.parameterData[-3][n]) #alpha
+	y =float(file.parameterData[-4][n]) #beta
 	z =float(file.parameterData[39][n]*10**6)
 	graph.SetPoint(n, x, y, z)
+	print(n,"	",x,"emit",z)
 
 #canv.SetLogz()
 
-graph.SetTitle('Final normalized x emitance, [#mum];#alpha_{x}, m;#beta_{x}, m')
+graph.SetTitle('Final normalized x emitance before 1st compressor, [#mum];#alpha_{x}, m;#beta_{x}, m')
 graph.Draw("COLZ")
 
-#betax = np.array(file.parameterData[-2]).reshape(20,20)
-#alphax = np.array(file.parameterData[-1]).reshape(20,20).T
-#enx = np.array(file.parameterData[39]).reshape(20,20).T
- 
+
+col1 = np.array(file.parameterData[-4])
+col2 = np.array(file.parameterData[-3])
+col3 = np.array(file.parameterData[-2])
+col4 = np.array(file.parameterData[-1])
+col5 = np.array(file.parameterData[39])*10**6
+
+#here is your data, in two numpy arrays
+
+data = np.array([col1, col2, col3, col4, col5])
+data = data.T
+#here you transpose your data, so to have it in two columns
+
+datafile_path = "./datafile.txt"
+with open(datafile_path, 'w+') as datafile_id:
+#here you open the ascii file
+	np.savetxt(datafile_id, data, fmt=['%f','%f','%f','%f','%f'])
+#here the ascii file is written. 
 
 
 input('done')
