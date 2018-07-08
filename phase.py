f33fc97738915d3c55232ef684e1569eeb46ae8f
FoxWise <bohdan96@gmail.com>
2018-07-09 01:36:48 +0300

    Calculated results. Pics appeared+neat axis titles

--- phase.py
+++ phase.py
@@ -19,7 +19,8 @@ y =np.array(file_emit.parameterData[39])*10**6 #emit
 n = len(file_phase.columnData[0][0])
 graph = TGraph(n,x,y)
 
-graph.SetTitle('Final normalized x emitance before 1st compressor, [#mum];#Psi, m;#Emit_{x}, mkm')
+graph.SetMarkerStyle(20)
+graph.SetTitle('Final normalized x emitance vs #Delta#Psi between BC;#Psi, rad;#epsilon_{n,x}, mkm')
 graph.Draw("AP")
 
 
